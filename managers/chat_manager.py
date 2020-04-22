4a045aaf799c0c52863bff5913293a531b4146a3
aburk3 <aburk3@gmail.com>
2020-04-22 06:41:28 -0500

    Removed method from chat_manager.

--- managers/chat_manager.py
+++ managers/chat_manager.py
@@ -6,10 +6,3 @@ def ask_who():
     user_input_lower = user_input.lower()
     given_name = user_input_lower.split()
     return given_name
-
-
-def ask_who():
-    user_input = input(f"What is your full name? ")
-    user_input_lower = user_input.lower()
-    given_full_name = user_input_lower.split()
-    return given_full_name
